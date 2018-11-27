c815573a17f9c578ac503383f3f4a23121443b13
unknown <Administrator@PC50500202412.domain.ocj.com.cn>
2018-11-27 16:09:34 +0800

    Initial commit

---
+++ configclient/src/main/java/com/zq/configclient/ConfigclientApplication.java
@@ -0,0 +1,23 @@
+package com.zq.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class ConfigclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigclientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/foo")
+    public String hi() {
+        return foo;
+    }
+}
